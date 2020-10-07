07a7062fa83ec3a93ad4bbbd26da4afeb0439e5a
ptrstpp950 <ptrstpp950@users.noreply.github.com>
2020-10-07 16:29:20 +0000

    jekyll build from Action 584784a68737298a4a13b6ca986dd13d713b51ec

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Liga Mistrzów i konta użytkowników!",
+        "excerpt":"Wyobraź sobie następującą sytuację. Pracujesz sobie w pewnej firmie i na drzwiach masz tabliczkę (do wyboru): “senior architekt”, “główny projektant” albo po prostu “Ten-Którego-Imienia-Nie-Wolno-Wymawiać” 😉. Przybiega do Ciebie szef wszystkich szefów, czyli jak wiadomo Krzysztof Jarzyna ze Szczecina i zaczyna monolog z następującym tekstem: “Nasi handlowcy odnieśli wczoraj ZAJE…ISTY sukces....","categories": ["champions-league"],
+        "tags": [],
+        "url": "https://poznajazure.pl/blog/champions-league/liga-mistrzow-i-konta-uzytkownikow/",
+        "teaser": null
+      },{
+        "title": "Liga Mistrzów i 7 sposobów na hosting!",
+        "excerpt":"Artykuł jest częścią, większej całości. W serii znajdują się: Część 1 - Liga Mistrzów i konta użytkowników Część 2 - Liga Mistrzów i 7 sposobów na hosting Część 3 - Liga Mistrzów i bazy danych Część 4 - Liga Mistrzów i brakujący element Część 5 - Liga Mistrzów i diagnostyka...","categories": ["champions-league"],
+        "tags": [],
+        "url": "https://poznajazure.pl/blog/champions-league/liga-mistrzow-i-7-sposobow-na-hosting/",
+        "teaser": null
+      },{
+        "title": "Liga Mistrzów i bazy danych",
+        "excerpt":"Artykuł jest częścią, większej całości. W serii znajdują się: Część 1 - Liga Mistrzów i konta użytkowników Część 2 - Liga Mistrzów i 7 sposobów na hosting Część 3 - Liga Mistrzów i bazy danych Część 4 - Liga Mistrzów i brakujący element Część 5 - Liga Mistrzów i diagnostyka...","categories": ["champions-league"],
+        "tags": [],
+        "url": "https://poznajazure.pl/blog/champions-league/liga-mistrzow-i-bazy-danych/",
+        "teaser": null
+      },{
+        "title": "Liga Mistrzów i brakujący element",
+        "excerpt":"Artykuł jest częścią, większej całości. W serii znajdują się: Część 1 - Liga Mistrzów i konta użytkowników Część 2 - Liga Mistrzów i 7 sposobów na hosting Część 3 - Liga Mistrzów i bazy danych Część 4 - Liga Mistrzów i brakujący element Część 5 - Liga Mistrzów i diagnostyka...","categories": ["champions-league"],
+        "tags": [],
+        "url": "https://poznajazure.pl/blog/champions-league/liga-mistrzow-i-brakujacy-element/",
+        "teaser": null
+      }]
